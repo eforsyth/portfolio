f0cedabc9470bf2ecdee8309c9f90b5d8eecd56d
eforsyth <euan@me.co.nz>
2022-04-01 15:06:42 +1300

    Update analysis.r

--- cycle_crashes/analysis.r
+++ cycle_crashes/analysis.r
@@ -4,6 +4,7 @@
 # libraries ---------------------------------------------------------------
 library(sf)
 library(tidyverse)
+library(arcpullr)  # accessing Esri REST API's
 library(spdep)
 library(tmap)
 
@@ -12,25 +13,14 @@ tmap_mode("view")
 
 # read in data ------------------------------------------------------------
 # parcels with yields
-data_rotorua_infill_parcels <- read_sf("U:/Home/Projects/ROT007.20 Eastside Assessment and HBA/Mapping/Shiny apps/Rotorua app data.gpkg",
-                                       'rotorua_capacity') %>% 
+data_crashes <- get_spatial_layer("https://services2.arcgis.com/JkPEgZJGxhSjYOo0/ArcGIS/rest/services/CycleRacks_View/FeatureServer/0") %>% 
+  st_transform(2193) 
   st_centroid()
 
-# create fishnet grid
-data_fishnet_grid <- st_make_grid(data_rotorua_infill_parcels,
-                                  cellsize = 200,
-                                  square = TRUE) %>% 
-  st_as_sf() %>% 
-  rowid_to_column() %>%
-  rename(grid_id = rowid,
-         geom = x) %>% 
-  mutate(grid_area_km2 = round(as.numeric(st_area(geom) / 1000), 2)) %>% 
-  relocate(geom, .after = last_col())
-
-# create hexagons
-data_fishnet_hexagons <- st_make_grid(data_rotorua_infill_parcels,
-                                      cellsize = 200,
-                                      square = FALSE)  %>% 
+# create hexbins
+data_hexbin <- st_make_grid(data_crashes,
+                            cellsize = 500,
+                            square = FALSE)  %>% 
   st_as_sf() %>% 
   rowid_to_column() %>% 
   rename(hex_id = rowid,
@@ -38,48 +28,25 @@ data_fishnet_hexagons <- st_make_grid(data_rotorua_infill_parcels,
   mutate(hex_area_km2 = round(as.numeric(st_area(geom) / 1000), 2)) %>% 
   relocate(geom, .after = last_col())
 
-# join yield to grid
-data_rotorua_yields_grid <- st_join(data_rotorua_infill_parcels,
-                                    data_fishnet_grid,
-                                    join = st_within) %>% 
-  as_tibble() %>% 
-  group_by(grid_id) %>% 
-  summarise(infill_yield = sum(Infill_Yield)) %>% 
-  right_join(data_fishnet_grid,
-             by = "grid_id")  %>% 
-  replace_na(list(infill_yield = 0)) %>% 
-  mutate(infill_yield_density_km2 = round(infill_yield / grid_area_km2, 2)) %>% 
-  relocate(infill_yield, .before = infill_yield_density_km2) %>% 
-  relocate(geom, .after = last_col()) %>% 
-  st_as_sf()
 
-# join yield to hex
-data_rotorua_yields_hex <- st_join(data_rotorua_infill_parcels,
-                                   data_fishnet_hexagons,
-                                   join = st_within) %>% 
+# join crashes to hexbins
+data_hexbin_crashes <- st_join(data_crashes,
+                               data_hexbin,
+                               join = st_within) %>% 
   as_tibble() %>% 
   group_by(hex_id) %>% 
-  summarise(infill_yield = sum(Infill_Yield)) %>% 
-  right_join(data_fishnet_hexagons,
+  summarise(crashes = sum(crashes)) %>% 
+  right_join(data_hexbin,
              by = "hex_id")  %>% 
-  replace_na(list(infill_yield = 0)) %>% 
-  mutate(infill_yield_density_km2 = round(infill_yield / hex_area_km2, 2)) %>% 
-  relocate(infill_yield, .before = infill_yield_density_km2) %>% 
+  replace_na(list(crashes = 0)) %>% 
+  mutate(crashes_density_km2 = round(crashes / hex_area_km2, 2)) %>% 
+  relocate(crashes, .before = crashes_density_km2) %>% 
   relocate(geom, .after = last_col()) %>% 
   st_as_sf()
 
 
 # create spatial weight matrix --------------------------------------------
-# for grid
-nb_grid <- poly2nb(data_rotorua_yields_grid, 
-              queen = TRUE)
-
-lw_grid <- nb2listw(neighbours = nb_grid, 
-               style = "W", 
-               zero.policy = TRUE)
-
-# for hex
-nb_hex <- poly2nb(data_rotorua_yields_hex, 
+nb_hex <- poly2nb(data_hexbin_crashes, 
                    queen = TRUE)
 
 lw_hex <- nb2listw(neighbours = nb_hex, 
@@ -87,35 +54,52 @@ lw_hex <- nb2listw(neighbours = nb_hex,
                     zero.policy = TRUE)
 
 # global Moran I ----------------------------------------------------------
-# apply to columns infill_yield or infill_yield_density_km2
-# grid infill_yield_density_km2 = 0.48
-# hex infill_yield_density_km2 = 0.53
-
-# for grid
-moran.mc(data_rotorua_yields_grid$infill_yield_density_km2,
-         listw = lw_grid, 
-         nsim = 999, 
-         zero.policy = TRUE)
-
-moran.plot(data_rotorua_yields_grid$infill_yield_density_km2,
-           listw = lw_grid, 
-           zero.policy = TRUE,
-           labels = data_rotorua_yields_grid$grid_id,
-           xlab = "Infill yield per km2",
-           ylab = "Neighbours infill yield per km2")
-
-# for hex
+# crashes_density_km2 = 0.nn
+# evidence of significant clustering across space
 moran.mc(data_rotorua_yields_hex$infill_yield_density_km2,
          listw = lw_hex, 
          nsim = 999, 
          zero.policy = TRUE)
 
+# default spdep Moran scatter plot
 moran.plot(data_rotorua_yields_grid$infill_yield_density_km2,
            listw = lw_grid, 
            zero.policy = TRUE,
            labels = data_rotorua_yields_grid$grid_id,
            xlab = "Infill yield per km2",
            ylab = "Neighbours infill yield per km2")
+
+# create a better looking Moran's I scatter plot with ggplot
+vis_LISA_scatter_plot <- vis_data_moran_plot_final %>% 
+  ggplot(aes(x = LVm2,
+             y = LVm2_lagged)) + 
+  geom_point(size = 2,
+             colour = "purple",
+             alpha = 0.1) +
+  geom_vline(xintercept = 2000,
+             linetype = "dashed") +
+  geom_hline(yintercept = 1800,
+             linetype = "dashed") +
+  geom_smooth(method = lm , 
+              color = "black", 
+              se = FALSE) +
+  labs(title = "Spatial autocorrelation of Auckland land values",
+       subtitle = "Global Moran's I returns a value of 0.286 for all parcels (p-value = 0.001)",
+       x = "Parcel LV/m2 ($)",
+       y = "Spatially lagged (500m) parcel LV/m2") +
+  theme_ipsum(grid = FALSE) +
+  facet_wrap(~UP_ZoneGroup)
+  
+vis_LISA_scatter_plot_marginal <- ggMarginal(vis_LISA_scatter_plot,
+                                             type = "histogram",
+                                             fill = "grey",
+                                             size = 10)
+
+
+vis_LISA_scatter_plot
+vis_LISA_scatter_plot_marginal
+
+
 
 
 # local Moran I -----------------------------------------------------------
@@ -139,28 +123,7 @@ localMoran_hex <- localmoran(data_rotorua_yields_hex$infill_yield_density_km2,
 p1 = 0.01
 p2 = 0.1
 
-# for grid
-results_rotorua_infill_grid_yields <- data_rotorua_yields_grid %>% 
-  mutate(LISA_infill_yield_rescale = scale(infill_yield_density_km2),
-         LISA_infill_yield_lag = lag.listw(lw_grid, 
-                                           LISA_infill_yield_rescale,
-                                           zero.policy = TRUE),
-         LISA_type = case_when(LISA_infill_yield_rescale >0 & LISA_infill_yield_lag >0 & localMoran_grid[,5] <= p1 ~ "High-high cluster - significant",
-                               LISA_infill_yield_rescale >0 & LISA_infill_yield_lag >0 & localMoran_grid[,5] <= p2 ~ "High-high cluster - less significant",
-                               LISA_infill_yield_rescale <0 & LISA_infill_yield_lag <0 & localMoran_grid[,5] <= p1 ~ "Low-low cluster - significant",
-                               LISA_infill_yield_rescale <0 & LISA_infill_yield_lag <0 & localMoran_grid[,5] <= p2 ~ "Low-low cluster - less significant",
-                               LISA_infill_yield_rescale >0 & LISA_infill_yield_lag <0 & localMoran_grid[,5] <= p1 ~ "High-low outlier - significant",
-                               LISA_infill_yield_rescale >0 & LISA_infill_yield_lag <0 & localMoran_grid[,5] <= p2 ~ "High-low outlier - less significant",
-                               LISA_infill_yield_rescale <0 & LISA_infill_yield_lag >0 & localMoran_grid[,5] <= p1 ~ "Low-high outlier - significant",
-                               LISA_infill_yield_rescale <0 & LISA_infill_yield_lag >0 & localMoran_grid[,5] <= p2 ~ "Low-high outlier - less significant",
-                               TRUE ~ as.character("Not-significant"))) %>% 
-  relocate(geom, .after = last_col())
-
-# summarise LISA classification
-table(results_rotorua_infill_parcel_yields$LISA_type)
-
-# for hex
-results_rotorua_infill_hex_yields <- data_rotorua_yields_hex %>% 
+results_hex_crashes <- data_rotorua_yields_hex %>% 
   mutate(LISA_infill_yield_rescale = scale(infill_yield_density_km2),
          LISA_infill_yield_lag = lag.listw(lw_hex, 
                                            LISA_infill_yield_rescale,
